c4b01de84718d248a28e966a7b8021858860beaf
Ashwin Umadi <ashwinumadi@engr2-2-68-132-dhcp.int.colorado.edu>
2024-10-14 09:31:21 -0600

    3

--- blanca_opentype_p.sh
+++ blanca_opentype_p.sh
@@ -56,7 +56,7 @@ echo "====== Running The Model ========"
 
 bash scripts/open_type_et_with_jobimtext.sh
 
-cd ../open_type/
+cd ../open_type_p/
 git clone https://github.com/uwnlp/open_type.git
 
 cd ./open_type
